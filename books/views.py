71f70bba9fa5f7e0330564ad6c76a4d2a9ebf1ad
CHR-onicles <tpandivine48@gmail.com>
2021-06-29 12:34:16 +0000

    added a template

--- books/views.py
+++ books/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Testing here')
+    return render(request, 'books/index.html')
