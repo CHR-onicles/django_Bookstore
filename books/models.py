2b36b66412a7a4eda527266d254462d978ace9e4
CHR-onicles <tpandivine48@gmail.com>
2021-06-30 12:11:51 +0000

    more models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    page_count = models.IntegerField(null=True)
+    thumbnail_url = models.CharField(max_length=256)
+    short_description = models.CharField(max_length=256)
+    long_description = models.TextField(null=True)
