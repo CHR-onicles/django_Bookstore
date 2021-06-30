04b2ab3d32b5eed20fc2adc4327ef94d047a2c47
CHR-onicles <tpandivine48@gmail.com>
2021-06-30 22:38:16 +0000

    still couldnt implement read more button

--- books/static/books/js/script.js
+++ books/static/books/js/script.js
@@ -1,16 +1,18 @@
-function readMoreFunc() {
-  // l_desc and l_desc_trunc are global variables that will be declared at the part of the HTML where the script is included
-
-  var long_desc = document.getElementById("long-desc");
+function readMore() {
+  /**************************
+  Document function here
+  **************************/
+  var long_desc_element = JSON.parse(document.getElementById("long-desc"));
+  var long_desc_text = long_desc_element.textContent;
   var btnText = document.getElementById("read-more");
 
-  if (long_desc.innerText.slice(long_desc.innerText.length - 1) === "…") {
-    console.log(long_desc.innerText.slice(long_desc.innerText.length - 1));
-    long_desc.innerHTML = l_desc;
+  if (long_desc_text.slice(long_desc_text.length - 1) === "…") {
+    console.log(long_desc_text.slice(long_desc_text.length - 1));
+    long_desc_text.textContent = 0;
     btnText.innerHTML = "Read less";
   } else {
-    console.log(long_desc.innerText.slice(long_desc.innerText.length - 1));
+    console.log(long_desc_text.slice(long_desc_text.length - 1));
     btnText.innerHTML = "Read more";
-    long_desc.innerText = l_desc_trunc;
+    long_desc_text.innerText = 0;
   }
 }
