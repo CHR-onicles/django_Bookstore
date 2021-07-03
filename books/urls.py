8fd6fce3fb4e464e97f1348190438685beea3452
CHR-onicles <tpandivine48@gmail.com>
2021-07-03 18:59:47 +0000

    more on authentication

--- books/urls.py
+++ books/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='home'),
-    path('book/<int:pk>', views.BookDetailView.as_view(), name='detail'),
+    path('<int:pk>', views.BookDetailView.as_view(), name='detail'),
     path('<int:id>/review', views.review, name='review'),
     path('<str:author>', views.author, name='author_books'),
 ]
--- django_Bookstore/urls.py
+++ django_Bookstore/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
+    path('login/', auth_view.LoginView.as_view(redirect_authenticated_user=True)),  # to prevent authenticated users from going to back to the login screen
     path('', include('django.contrib.auth.urls' )),
 ]
